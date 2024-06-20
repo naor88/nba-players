4759d7a51ca4952ffb7c37ac25da601ba9f1751b
Naor Ben David <bdnaor@gmail.com>
2024-06-21 02:24:41 +0300

    refactor: ui

--- src/components/ApiKeyInput.tsx
+++ src/components/ApiKeyInput.tsx
@@ -11,22 +11,23 @@ const ApiKeyInput: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="card w-96 bg-white shadow-xl">
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="card w-96 bg-white shadow-xl text-black">
         <div className="card-body">
           <h2 className="card-title">API Key Required</h2>
           <p>Please enter your API key to continue.</p>
           <form onSubmit={handleSubmit}>
             <div className="form-control">
-              <label className="label">
-                <span className="label-text">API Key</span>
+              <label className="label cursor-pointer" htmlFor="key_input">
+                <span className="label-text text-black">API Key</span>
               </label>
               <input
+                id="key_input"
                 type="text"
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
                 placeholder="Enter API key"
-                className="input input-bordered w-full"
+                className="input input-bordered w-full focus:border-primary focus:border-4 text-white"
               />
             </div>
             <div className="form-control mt-4">
@@ -38,14 +39,14 @@ const ApiKeyInput: React.FC = () => {
           <p className="mt-4 text-sm">
             <span>Don't have an API key? </span>
             <span>
-              You can obtain an API key by creating a free account on{" "}
+              You can obtain an API key by creating a free account at{" "}
+              <a
+                href="https://app.balldontlie.io"
+                className="text-blue-500 underline"
+              >
+                https://app.balldontlie.io
+              </a>
             </span>
-            <a
-              href="https://app.balldontlie.io"
-              className="text-blue-500 underline"
-            >
-              https://app.balldontlie.io
-            </a>
           </p>
         </div>
       </div>
