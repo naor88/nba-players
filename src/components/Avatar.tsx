f7a30cb90f5a4aa27dfd45ca732627c69c779291
Naor Ben David <bdnaor@gmail.com>
2024-06-21 01:36:14 +0300

    refactor: build

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { IPlayer } from "../types";
 import { getPlayerAvatar } from "../utils";
-
+import profileImg from "../assets/profile-user-icon.jpg";
 interface AvatarProps {
   player: IPlayer;
 }
 
 const Avatar: React.FC<AvatarProps> = ({ player }) => {
   const [hasError, setHasError] = useState(false);
-  const [imageURL, setImageURL] = useState('');
+  const [imageURL, setImageURL] = useState("");
 
   const handleError = () => {
     setHasError(true);
   };
-
 
   useEffect(() => {
     const fetchImageURL = async () => {
@@ -22,13 +21,17 @@ const Avatar: React.FC<AvatarProps> = ({ player }) => {
         setImageURL(urls.strCutout);
       } catch (error) {
         console.error("Failed to fetch player avatar:", error);
-        setImageURL("/profile-user-icon.jpg");
+        // setImageURL("/profile-user-icon.jpg");
         setHasError(true);
       }
     };
 
     fetchImageURL();
   }, [player]);
+
+  if (hasError) {
+    return <img src={profileImg} />;
+  }
 
   return (
     <img
